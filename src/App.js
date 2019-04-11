0562f9a937dbe8c79be66b5f70efeaa1c27ea2fd
Sherill Samuel <sherill.samuel@gmail.com>
2019-04-10 20:45:26 -0400

    Counter application is at a working state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, {Component} from 'react';
 
-class App extends Component {
-  render() {
+class Counter extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    }
+  }
+
+
+  render () {
     return (
-    );
+      <div className="container">
+        <div className="navbar">Counter.js</div>
+        <div className="counter">
+          <h1>What do we put here?</h1>
+          <button type="button">Increment</button>
+          <button type="button">Decrement</button>
+        </div>
+      </div>
+    )
   }
 }
 
-export default App;
+export default Counter;
